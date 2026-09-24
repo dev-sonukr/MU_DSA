921964e451fc93e01cb57db7932c6c4289ed67c7
Sonu Kumar <sonukumarraj2005@gmail.com>
2026-09-24 11:50:19 +0530

    varable

---
+++ BASIC/variable.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int age = 100;
+    float marks = 33;
+    char grade = 'E';
+    bool pass = true;
+    string name = "Sonu";
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Marks: " << marks << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Pass: " << pass << endl;
+
+    return 0;
+}
